305851cfb9e9b87ae7a6a83bdfcd18a0bebf822b
jupmorenor@gmail.com <jupmorenor@gmail.com@c95550d5-de2d-7008-4073-251e15fe0031>
2014-06-06 22:54:36 +0000

    Implementacion de la primera parte de la funcion "transformada"

--- core/implementacion.py
+++ core/implementacion.py
@@ -5,3 +5,62 @@ Created on 3/06/2014
 @author:
 @author:
 '''
+from __future__ import print_function
+from PIL import Image
+import numpy as np
+import pyfftw as pFT
+
+_ruta = []
+_formatos = ['PNG', 'JPEG', 'GIF', 'BMP']
+
+def _abrirImagen():
+    
+    imagenes = []
+    if len(_ruta) == 1:
+        imagenes = Image.open(_ruta.pop())
+    elif len(_ruta)>1:
+        for img in _ruta:
+            imagenes.append(Image.open(img))
+    else:
+        imagenes = None
+    return imagenes
+
+def estabecerRuta(ruta):
+    _ruta = ruta
+
+def transformada():
+    """
+    Calcula la Transformada Discreta de Fourier de una imagen RGB o en escala de grises 
+    y retorna en forma de imagenes la parte reeal, parte imaginaria, magnitud y angulo fase
+    """
+    imagen = _abrirImagen()
+    if imagen.format in _formatos:
+        if imagen.mode == 'RGB' or imagen.mode == 'RGBA':
+            datos = np.asarray(imagen)/255.0
+            transf = pFT.builders.fftn(datos, datos.shape)
+            resultado = transf()
+            ptReal = Image.fromarray(np.around(resultado.real), 'RGB')
+            ptImag = Image.fromarray(np.around(resultado.imag), 'RGB')
+            magnitud = Image.fromarray(np.around(np.absolute(resultado)), 'RGB')
+            fase = Image.fromarray(np.around(np.rad2deg(np.arctan2(resultado.imag, resultado.real))), 'RGB')
+            return ptReal, ptImag, magnitud, fase
+    
+
+def inversa():
+    imagenes = _abrirImagen()
+    for imagen in imagenes:
+        pass
+
+class NoImagenError(Exception):
+    pass
+
+def _print(lista):
+    for fila in lista:
+        for columna in fila:
+            print(columna, end='\t')
+        print("")
+
+if __name__ == '__main__':
+    _ruta = ["D:\\Juanpa\\Ingenieria de Sistemas UD\\Semestre V\\Matematicas especiales\\transformadas+\\XD.png"]
+    print(transformada()) 
+    
